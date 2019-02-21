4b50b72aa701ba5d98cb081544dc9dc287e2e2b2
Samad Alias Nyein Chan <samadanc@mun.ca>
2019-02-21 00:28:32 -0330

    with login and database working

--- Blog/forms.py
+++ Blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +14,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filer_by(username=username.data).first()
+        if user:
+            raise ValidationError('Be unique. Choose a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filer_by(email=email.data).first()
+        if user:
+            raise ValidationError('Your email is already registered with us.')
 
 
 class LoginForm(FlaskForm):
